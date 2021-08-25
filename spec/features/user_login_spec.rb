cfe843c21038e839f34df644b2f2346cc5887666
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-08-24 20:58:42 -0700

    add initial sample test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
-RSpec.feature "UserLogins", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to product detail page from home page by clicking on a product", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+    end
+  end
+
+  scenario "Add the first product to cart" do
+    visit root_path
+    first("article.product").hover.click_on "Add"
+    
+    expect(page).to have_content 'My Cart (1)'
+
+    # uncomment to see screenshot
+    # save_screenshot
+  end
 end
